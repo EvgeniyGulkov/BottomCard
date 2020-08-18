17436e0d606a22d2530528b88b304153e1852161
Evgeniy <ewgeniy2004@list.ru>
2020-08-18 19:19:18 +0700

    fixes

--- BottomCard/BottomCard.h
+++ BottomCard/BottomCard.h
@@ -8,11 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for BottomCardView.
-FOUNDATION_EXPORT double BottomCardViewVersionNumber;
+//! Project version number for BottomCard.
+FOUNDATION_EXPORT double BottomCardVersionNumber;
 
-//! Project version string for BottomCardView.
-FOUNDATION_EXPORT const unsigned char BottomCardViewVersionString[];
+//! Project version string for BottomCard.
+FOUNDATION_EXPORT const unsigned char BottomCardVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BottomCardView/PublicHeader.h>
 
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'BottomCard' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BottomCard
+  pod 'pop'
+
+end
+
+target 'BottomCardExample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BottomCardExample
+  pod 'pop'
+end

acfb6cea3d34dbb0a9283eca2626b17ee4d2080d
Evgeniy <ewgeniy2004@list.ru>
2020-08-18 17:36:35 +0700

    add framework

---
+++ BottomCardView/BottomCardView.h
@@ -0,0 +1,19 @@
+//
+//  BottomCardView.h
+//  BottomCardView
+//
+//  Created by Gulkov on 18.08.2020.
+//  Copyright © 2020 Gulkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BottomCardView.
+FOUNDATION_EXPORT double BottomCardViewVersionNumber;
+
+//! Project version string for BottomCardView.
+FOUNDATION_EXPORT const unsigned char BottomCardViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BottomCardView/PublicHeader.h>
+
+
